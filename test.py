e3f9adb940c27c360b7509c5e82b47271ac02739
I RAMANAGENDRA <ramanagendra221@gmail.com>
2025-01-05 11:37:42 +0530

    test.py updated

--- test.py
+++ test.py
@@ -8,8 +8,14 @@ def calcualate(x,y):
     return(x+y)
 
 if __name__ == "__main__":
-    x,y=100,200
-    result=calcualate(x,y)
-    print(f"Here is my result {result}")
+    with mlflow.start_run():
+        x,y=100,200
+        result=calcualate(x,y)
+        print(f"Here is my result {result}")
+        mlflow.log_param("x", x)
+        mlflow.log_param("y", y)
+        mlflow.log_param("result", result)
+
+    
     
     
